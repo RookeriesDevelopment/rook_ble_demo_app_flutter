207fb267156c21de11c74a4b4f5f363e4c04e4ad
dnieln7 <dnieln7@gmail.com>
2022-12-28 14:01:34 -0600

    Added BLE android configuration

--- android/app/src/main/kotlin/com/rookmotion/rook_ble_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/rookmotion/rook_ble_demo/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.rookmotion.rook_ble_demo
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.polidea.rxandroidble2.exceptions.BleException
 import io.flutter.embedding.android.FlutterActivity
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        RxJavaPlugins.setErrorHandler { throwable ->
+            if (throwable is UndeliverableException && throwable.cause is BleException) {
+                return@setErrorHandler // ignore BleExceptions since we do not have subscriber
+            } else {
+                throw throwable
+            }
+        }
+    }
 }
